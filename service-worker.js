c69381eb332c291529b942b08b16ac59c01ed0be
osakanblue-ctrl <osakanblue@gmail.com>
2026-03-06 02:02:21 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,9 @@
-const CACHE = 'steelguitarfx-v12';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './synth.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,900;1,700&family=IBM+Plex+Mono:wght@400;600&display=swap'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
+// Disabled - no caching
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
